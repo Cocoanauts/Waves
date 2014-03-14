e660aa1cf3bf94a21f11991c37181bc1478734f7
Elvis Nunez <elvisnunez@me.com>
2014-03-14 23:01:18 +0100

    Add hardcoded css.

--- WaveApp/WaveApp/WAVEWebViewDelegate.m
+++ WaveApp/WaveApp/WAVEWebViewDelegate.m
@@ -8,16 +8,41 @@
 
 #import "WAVEWebViewDelegate.h"
 
+@interface WAVEWebViewDelegate ()
+@end
+
 @implementation WAVEWebViewDelegate
 
 static NSString * const kMainURL = @"http://www.facebook.com/messages";
 
-@synthesize webView;
-
 - (void)awakeFromNib
 {
+    self.webView.frameLoadDelegate = self;
     [self.webView setAcceptsTouchEvents:YES];
 	[self.webView setMainFrameURL:kMainURL];
+}
+
+- (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame
+{
+    DOMDocument *domDocument = [sender mainFrameDocument];    
+    DOMElement *styleElement = [domDocument createElement:@"style"];
+    [styleElement setAttribute:@"type" value:@"text/css"];
+    
+    NSString *string = [NSString stringWithFormat:@"#fbDockChatBuddylistNub,"
+                                                    "#pagelet_bluebar,"
+                                                    "#leftColContainer,"
+                                                    "#rightCol,"
+                                                    "#headerArea,"
+                                                    "._2nd,"
+                                                    "._3j5,"
+                                                    "#pagelet_chat { display: none !important; }"
+                                                    "#pagelet_group_mall { padding: 0px 5px !important; }"
+                                                    ".storyContent .uiUfi { width: 100% !important; }"];
+    
+    DOMText *cssText = [domDocument createTextNode:string];
+    [styleElement appendChild:cssText];
+    DOMElement *headElement = (DOMElement*)[[domDocument getElementsByTagName:@"head"] item:0];
+    [headElement appendChild:styleElement];
 }
 
 @end
