44ac9b15a572ca857fa6bc5801c3dcb13d3d3961
Elvis Nunez <elvisnunez@me.com>
2014-03-16 00:54:29 +0100

    Load and save CSS to plist.

--- WaveApp/WaveApp/Preferences/WAVECSSPreferencesViewController.m
+++ WaveApp/WaveApp/Preferences/WAVECSSPreferencesViewController.m
@@ -8,8 +8,8 @@
 
 #import "WAVECSSPreferencesViewController.h"
 
-@interface WAVECSSPreferencesViewController ()
-
+@interface WAVECSSPreferencesViewController () <NSTextViewDelegate>
+@property (unsafe_unretained) IBOutlet NSTextView *textView;
 @end
 
 @implementation WAVECSSPreferencesViewController
@@ -20,6 +20,18 @@
     if (self) {
     }
     return self;
+}
+
+- (void)awakeFromNib
+{
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"preferences" ofType:@"plist"];
+    NSData *plistXML = [[NSFileManager defaultManager] contentsAtPath:plistPath];
+    NSString *errorDesc = nil;
+    NSPropertyListFormat format;
+    NSDictionary *preferencesDict = (NSDictionary *)[NSPropertyListSerialization propertyListFromData:plistXML mutabilityOption:NSPropertyListMutableContainersAndLeaves format:&format errorDescription:&errorDesc];
+    if (preferencesDict) {
+        self.textView.string = [preferencesDict objectForKey:@"CSS"];
+    }    
 }
 
 #pragma mark - MASPreferencesViewController
@@ -37,6 +49,32 @@
 - (NSString *)toolbarItemLabel
 {
     return NSLocalizedString(@"CSS", @"Toolbar item name for the CSS preference pane");
+}
+
+#pragma mark - NSTextViewDelegate
+
+- (void)textDidChange:(NSNotification *)aNotification
+{
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"preferences" ofType:@"plist"];
+    NSData *plistXML = [[NSFileManager defaultManager] contentsAtPath:plistPath];
+    NSString *errorDesc = nil;
+    NSPropertyListFormat format;
+    NSDictionary *preferencesDict = (NSDictionary *)[NSPropertyListSerialization propertyListFromData:plistXML mutabilityOption:NSPropertyListMutableContainersAndLeaves format:&format errorDescription:&errorDesc];
+    if (!preferencesDict) {
+        NSLog(@"Error reading plist: %@, format: %lu", errorDesc, format);
+    }
+    [preferencesDict setValue:self.textView.string forKey:@"CSS"];
+    
+    NSString *error = nil;
+    // create NSData from dictionary
+    NSData *plistData = [NSPropertyListSerialization dataFromPropertyList:preferencesDict format:NSPropertyListXMLFormat_v1_0 errorDescription:&error];
+    
+    // check is plistData exists
+    if(plistData) {
+        [plistData writeToFile:plistPath atomically:YES];
+    } else {
+        NSLog(@"Error in saveData: %@", error);
+    }
 }
 
 @end
